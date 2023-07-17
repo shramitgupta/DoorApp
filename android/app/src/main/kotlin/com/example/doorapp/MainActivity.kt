5bb16688e2998fe576ee7538bc11781ee5173e5a
shramitgupta <shramitgupta1212@gmail.com>
2023-07-17 12:52:57 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/doorapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.doorapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/admin_signup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminSignUp extends StatelessWidget {
+  const AdminSignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
